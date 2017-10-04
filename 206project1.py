7334e0682b7f8c1fae1abf6ee1f2b71fd12ac0f5
Adam Benson <adambens@umich.edu>
2017-10-03 20:02:44 -0400

    Adding completed project

--- 206project1.py
+++ 206project1.py
@@ -1,5 +1,9 @@
 import os
+import csv
 import filecmp
+from operator import itemgetter
+#Adam Benson
+
 
 def getData(file):
 #Input: file name
@@ -12,7 +16,19 @@ def getData(file):
 #cases.
 
 	#Your code here:
-	pass
+	f = open(file, newline='')
+	a = f.readlines()
+	dlist = []
+	for line in a[1:]:
+		x = line.split(',')
+		d = {}
+		d["First"] = x[0]
+		d["Last"] = x[1]
+		d["Email"] = x[2]
+		d["Class"] = x[3]
+		d["DOB"] = x[4]
+		dlist.append(d)
+	return dlist
 
 #Sort based on key/column
 def mySort(data,col):
@@ -20,7 +36,15 @@ def mySort(data,col):
 #Output: Return a string of the form firstName lastName
 
 	#Your code here:
-	pass
+	x = sorted(data, key=itemgetter(col))
+	y = x[0]
+	if col == 'Last':
+		return y['First'] + " " + y[col]
+	elif col == 'First':
+		return y[col] + " " + y['Last']
+	elif col == 'Email':
+		return y['First'] + " " + y['Last']
+
 
 #Create a histogram
 def classSizes(data):
@@ -30,7 +54,26 @@ def classSizes(data):
 # [('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)]
 
 	#Your code here:
-	pass
+	freshman = 0
+	sophomore = 0
+	junior = 0
+	senior = 0
+	for row in data:
+		if row['Class'] == 'Freshman':
+			freshman += 1
+		elif row['Class'] == 'Sophomore':
+			sophomore += 1
+		elif row['Class'] == 'Junior':
+			junior += 1
+		elif row['Class'] == 'Senior':
+			senior +=1 
+		else:
+			pass
+		#print("No Grade Entered")
+	#print(row)
+	grades = [('Freshman', freshman), ('Sophomore', sophomore), ('Junior', junior), ("Senior", senior)]
+	ordered = sorted(grades, key=lambda x: x[1], reverse=True)
+	return(ordered)
 
 
 
@@ -41,7 +84,22 @@ def findDay(a):
 # most often seen in the DOB
 
 	#Your code here:
-	pass
+	day_count = {}
+	for day in a:
+		bd = day['DOB']
+		day = bd.split('/')
+		x = day[1]
+		if x not in day_count:
+			day_count[x] = 1
+		else:
+			day_count[x] += 1
+	day_lst = []
+	for key in day_count.keys():
+		day_tup = (key, day_count[key])
+		day_lst.append(day_tup)
+	sorted_lst = sorted(day_lst, key = lambda x: x[1], reverse = True)
+	return int(sorted_lst[0][0])
+
 
 
 # Find the average age (rounded) of the Students
@@ -51,7 +109,24 @@ def findAge(a):
 # most often seen in the DOB
 
 	#Your code here:
-	pass
+	agedic = {}
+	current = 2017
+	totalage = 0
+	numpeople = 0
+	for line in a:
+		x = line['DOB']
+		o = x.split('/')
+		p = o[2][:4]
+		#print(int(p))
+		totalage += int(p)
+		numpeople += 1
+	#print(totalage)
+	#print(numpeople)
+
+	mean = totalage / numpeople
+	average = 2017 - mean
+	return int(average)
+
 
 #Similar to mySort, but instead of returning single
 #Student, all of the sorted data is saved to a csv file.
@@ -60,8 +135,11 @@ def mySortPrint(a,col,fileName):
 #Output: None
 
 	#Your code here:
-	pass
-
+	data = sorted(a, key = lambda x: x[col])
+	new = open(fileName, 'w')
+	for t in data:
+		new.write('{},{},{}\n'.format(t['First'], t['Last'], t['Email']))
+	new.close()
 
 
 ################################################################
